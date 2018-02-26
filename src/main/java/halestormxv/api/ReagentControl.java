f557cd4bb6d02da167f4c5ac461e3bdc4e9306dc
Unknown <HalestormXV@gmail.com>
2018-02-26 14:34:39 -0500

    Major overhaul to the reagent consumption check to use the ItemHandler now and merged checking the Rune Bag and the Inventory of the player into one function.

--- src/main/java/halestormxv/api/ReagentControl.java
+++ src/main/java/halestormxv/api/ReagentControl.java
@@ -1,21 +1,15 @@
 package halestormxv.api;
 
 import halestormxv.capabilities.runebag.RuneBagProvider;
-import halestormxv.init.ItemInit;
 import halestormxv.objects.items.RuneBag;
-import halestormxv.utils.Logging;
 import halestormxv.utils.interfaces.IRuneBagProvider;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.ItemStackHandler;
-
-import java.util.stream.Stream;
 
 import static halestormxv.utils.Logging.getLogger;
 
@@ -25,6 +19,89 @@ public class ReagentControl
         Item reagentItem = itemStack.getItem();
         entityLiving.inventory.clearMatchingItems(reagentItem, meta, reagentCost, null);
     }
+
+    public static boolean checkAndConsumeReagent(EntityPlayer thePlayer, ItemStack itemStack, int reagentCost)
+    {
+        int total;
+        Item reagentItem;
+        IItemHandler playerInventory = thePlayer.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+        IRuneBagProvider theRuneBag = thePlayer.getCapability(RuneBagProvider.RUNEBAG_CAP, null);
+        reagentItem = itemStack.getItem();
+        IItemHandler bagInventory = theRuneBag.getBag(EnumDyeColor.WHITE);
+        boolean hasReagent = thePlayer.inventory.hasItemStack(itemStack);
+        if(hasReagent)
+        {
+            for (int slot = 0; slot < playerInventory.getSlots(); slot++)
+            {
+                ItemStack stack = playerInventory.getStackInSlot(slot);
+                if (!stack.isEmpty() && stack.getItem().equals(reagentItem))
+                {
+                    total = stack.getCount();
+                    if (total >= reagentCost)
+                    {
+                        stack.shrink(reagentCost);
+                        getLogger().info("Player has " + (total-reagentCost) +" "+ stack.getDisplayName() +
+                                " in the slot cause I ate "+reagentCost);
+                        return true;
+                    }
+                }
+            }
+        }else{
+            for (int slot = 0; slot < playerInventory.getSlots(); slot++)
+            {
+                ItemStack stack = playerInventory.getStackInSlot(slot);
+                if (!stack.isEmpty() && stack.getItem() instanceof RuneBag)
+                {
+                    reagentItem = itemStack.getItem();
+                    for (int bagSlots = 0; bagSlots < bagInventory.getSlots(); bagSlots++)
+                    {
+                        ItemStack stackInBagSlot = bagInventory.getStackInSlot(bagSlots);
+                        if (!stackInBagSlot.isEmpty() && stackInBagSlot.getItem().equals(reagentItem))
+                        {
+                            total = stackInBagSlot.getCount();
+                            if (total >= reagentCost)
+                            {
+                                bagInventory.getStackInSlot(bagSlots).shrink(reagentCost);
+                                getLogger().info("Player has " + (total-reagentCost) +" "+ stackInBagSlot.getDisplayName() +
+                                        " in the Rune Bag cause I ate "+reagentCost);
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int checkForReagentQuantityRuneBag(ItemStack itemStack, EntityPlayer thePlayer, int reagentCost)
+    {
+        int total = 0;
+        Item reagentItem;
+        IItemHandler itemHandler = thePlayer.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+        for (int slot = 0; slot < itemHandler.getSlots(); slot++)
+        {
+            ItemStack stack = itemHandler.getStackInSlot(slot);
+            if (!stack.isEmpty() && stack.getItem() instanceof RuneBag)
+            {
+                IRuneBagProvider theRuneBag = thePlayer.getCapability(RuneBagProvider.RUNEBAG_CAP, null);
+                reagentItem = itemStack.getItem();
+                IItemHandler bagHandler = theRuneBag.getBag(EnumDyeColor.WHITE);
+                for (int bagSlots = 0; bagSlots < bagHandler.getSlots(); bagSlots++)
+                {
+                    ItemStack stackInBagSlot = bagHandler.getStackInSlot(bagSlots);
+                    if (!stackInBagSlot.isEmpty() && stackInBagSlot.getItem().equals(reagentItem))
+                    {
+                        total = stackInBagSlot.getCount();
+                        bagHandler.getStackInSlot(bagSlots).shrink(reagentCost);
+                        return total;
+                    }
+                }
+            }
+        }
+        return total;
+    }
+
 
     public static int checkForReagentQuantity(ItemStack itemStack, EntityPlayer player) {
         int count = 0;
@@ -44,43 +121,5 @@ public class ReagentControl
         }
         return 0;
     }
-
-    //Work on This Some More - almost got it? Maybe?
-    public static int checkForReagentQuantityRuneBag(ItemStack itemStack, EntityPlayer thePlayer)
-    {
-        int total = 0;
-        System.out.println("Good news is I am getting called at least.");
-        Item reagentItem;
-        for (int slot = 0; slot < thePlayer.inventory.getSizeInventory(); slot++)
-        {
-            ItemStack stack = thePlayer.inventory.getStackInSlot(slot);
-            if (!stack.isEmpty() && stack.getItem() instanceof RuneBag)
-            {
-                //LEFT OFF OVER HERE\\
-                getLogger().info("I found the rune bag while going through all the slots!");
-                NBTTagCompound bagData = stack.serializeNBT();
-                getLogger().info(bagData);
-                IRuneBagProvider theRuneBag = stack.getCapability(RuneBagProvider.RUNEBAG_CAP, null);
-                theRuneBag.getBag(EnumDyeColor.WHITE);
-                reagentItem = itemStack.getItem();
-                IItemHandler bagHandler = new ItemStackHandler(theRuneBag.getBag(EnumDyeColor.WHITE).getSlots());
-                for (int bagSlots = 0; bagSlots < bagHandler.getSlots(); bagSlots++)
-                {
-                    getLogger().info("I made it all the way into the bag!");
-                    ItemStack stackInBagSlot = bagHandler.getStackInSlot(bagSlots);
-                    if (!stackInBagSlot.isEmpty() && stackInBagSlot.getItem().equals(reagentItem))
-                    {
-                        total = stackInBagSlot.getCount();
-                        System.out.println("Player has: " + total + stackInBagSlot.getDisplayName() + " in the Rune Bag.");
-                        return total;
-                    }
-                }
-            } else {
-                getLogger().info("Sorry Kappa, you almost got it.");
-            }
-            getLogger().info("Loopy loopy loop. You have a problem in your loop!");
-        }
-        getLogger().info("Sorry Kappa, no Rune Bag here.");
-        return total;
-    }
 }
+
--- src/main/java/halestormxv/objects/items/staffs/abilities/Buff_Resistance.java
+++ src/main/java/halestormxv/objects/items/staffs/abilities/Buff_Resistance.java
@@ -16,42 +16,27 @@ public class Buff_Resistance {
     //IRuneCraftLevel runeCraftLevel = playerIn.getCapability(rcLvl_Provider.RUNECRAFT_LEVEL, null);
     //Make a Caster Level Perhaps?
 
-    public static void applyResistanceSelf(World worldIn, EntityPlayer playerIn, EnumHand handIn, ItemStack reagentRequired, int reagentCost) {
+    public static void applyResistanceSelf(World worldIn, EntityPlayer playerIn, EnumHand handIn, ItemStack reagentRequired, int reagentCost)
+    {
         ItemStack heldItem = playerIn.getHeldItem(handIn);
-        boolean hasItemInInventory = playerIn.inventory.hasItemStack(new ItemStack(reagentRequired.getItem(), reagentCost, reagentRequired.getMetadata()));
-        if (hasItemInInventory)
+        boolean passed = ReagentControl.checkAndConsumeReagent(playerIn, reagentRequired, reagentCost);
+        if(passed)
         {
-            if (ReagentControl.checkForReagentQuantity(reagentRequired, playerIn) >= reagentCost) {
-                playerIn.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("resistance"), 1800, 1));
-                ReagentControl.consumeReagent(reagentRequired, reagentRequired.getMetadata(), reagentCost, worldIn, playerIn);
-                heldItem.damageItem(1, playerIn);
-            } else {
-                PacketChatUtils.sendNoSpam(playerIn, "\u00A74This ability requires " + reagentCost + " " + reagentRequired.getDisplayName() + "(s) to cast.");
-            }
-        } else {
-            int maybeInRuneBag = ReagentControl.checkForReagentQuantityRuneBag(reagentRequired, playerIn);
-            if (maybeInRuneBag >= reagentCost)
-            {
-                playerIn.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("resistance"), 1800, 1));
-                ReagentControl.consumeReagent(reagentRequired, reagentRequired.getMetadata(), reagentCost, worldIn, playerIn);
-                heldItem.damageItem(1, playerIn);
-            } else {
-                PacketChatUtils.sendNoSpam(playerIn, "\u00A74This ability requires " + reagentCost + " " + reagentRequired.getDisplayName() + "(s) to cast.");
-            }
+            playerIn.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("resistance"), 1800, 1));
+            heldItem.damageItem(1, playerIn);
+        }else{
+            PacketChatUtils.sendNoSpam(playerIn, "\u00A74This ability requires " + reagentCost + " " + reagentRequired.getDisplayName() + "(s) to cast.");
         }
     }
 
     public static void applyFireResistance(World worldIn, EntityPlayer playerIn, EnumHand handIn, ItemStack reagentRequired, int reagentCost) {
         ItemStack heldItem = playerIn.getHeldItem(handIn);
-        if (playerIn.inventory.hasItemStack(new ItemStack(reagentRequired.getItem(), reagentCost, reagentRequired.getMetadata()))) {
-            if (ReagentControl.checkForReagentQuantity(reagentRequired, playerIn) >= reagentCost) {
-                playerIn.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("fire_resistance"), 1800, 1));
-                ReagentControl.consumeReagent(reagentRequired, reagentRequired.getMetadata(), reagentCost, worldIn, playerIn);
-                heldItem.damageItem(1, playerIn);
-            } else {
-                PacketChatUtils.sendNoSpam(playerIn, "\u00A74This ability requires " + reagentCost + " " + reagentRequired.getDisplayName() + "(s) to cast.");
-            }
-        } else {
+        boolean passed = ReagentControl.checkAndConsumeReagent(playerIn, reagentRequired, reagentCost);
+        if(passed)
+        {
+            playerIn.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("fire_resistance"), 1800, 1));
+            heldItem.damageItem(1, playerIn);
+        }else{
             PacketChatUtils.sendNoSpam(playerIn, "\u00A74This ability requires " + reagentCost + " " + reagentRequired.getDisplayName() + "(s) to cast.");
         }
     }
